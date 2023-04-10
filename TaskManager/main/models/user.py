5a0be2cbd7c29f06b3f216a8cf36e511a604437c
denis <funiusa@gmail.com>
2023-04-10 14:25:22 +0500

    Remove unused code

--- TaskManager/main/models/user.py
+++ TaskManager/main/models/user.py
@@ -1,30 +1,5 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password, **extra_fields):
-        if not username:
-            raise ValueError("The Username field must be set.")
-        if not email:
-            raise ValueError("The Email field must be set.")
-
-        email = self.normalize_email(email)
-        user = self.model(
-            username=username, email=email, password=password, **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        return self.create_user(
-            username=username, email=email, password=password, **extra_fields
-        )
 
 
 class User(AbstractUser):
@@ -36,7 +11,6 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=255, default=Roles.DEVELOPER, choices=Roles.choices, db_index=True
     )
-    # objects = CustomUserManager()
     password = models.CharField(max_length=128, blank=True)
     REQUIRED_FIELDS = ["role", "email"]
 
