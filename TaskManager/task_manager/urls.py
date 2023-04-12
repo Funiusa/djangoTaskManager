af94e6838ed0cc15d2234829b2d9b72e7984a0f2
denis <funiusa@gmail.com>
2023-04-12 18:09:51 +0500

    Connection simple JWT

--- TaskManager/task_manager/urls.py
+++ TaskManager/task_manager/urls.py
@@ -21,6 +21,10 @@ from main.views import UserViewSet, TaskViewSet, TagViewSet
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -63,4 +67,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
