6d4569f07579a5c8acde58002825d33b7b7211e3
denis <funiusa@gmail.com>
2023-04-12 17:42:52 +0500

    Add phone and birth in User model

--- TaskManager/main/models/user.py
+++ TaskManager/main/models/user.py
@@ -7,7 +7,8 @@ class User(AbstractUser):
         DEVELOPER = "developer"
         MANAGER = "manager"
         ADMIN = "admin"
-
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
     role = models.CharField(
         max_length=255, default=Roles.DEVELOPER, choices=Roles.choices, db_index=True
     )
